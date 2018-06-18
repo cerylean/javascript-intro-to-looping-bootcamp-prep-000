bcc7e4b6d2dbd046bcab5ba8bc418639af87aadd
Lyssa Treibitz <cerylean@gmail.com>
2018-06-18 19:30:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,7 +18,8 @@ function doWhileLoop (array) { function maybeTrue() {
   return Math.random() >= 0.5
 }
   do {
-  console.log('doo-bee-doo-bee-doo')
+  array.pop()
 } while (array.length > 0 && maybeTrue());
   return array}
+
 
